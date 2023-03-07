a3efd952c5666f27edd8e771981257a55858bb2d
unknown <joakindel95@gmail.com>
2023-03-07 15:29:45 -0300

    import img

--- src/pages/TipoDeAnalisis.jsx
+++ src/pages/TipoDeAnalisis.jsx
@@ -1,35 +1,38 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import imgBase from "/public/base.png"
+import imgVigas from "/public/vigas.png"
+import imgVigasTubo from "/public/vigas-tubo.png"
 
 const TipoDeAnalisis = () => {
-  const [urlImg, setUrlImg] = useState("public/base.png");
+  const [urlImg, setUrlImg] = useState(imgBase);
 
   return (
     <div className="my-10 mx-10 lg:mx-48 max-w-5xl ">
       <h2 className="text-3xl font-black text-barbieriBlue border-b pb-4 border-ligthGray">
         Analisis de carga
       </h2>
-      <div className="flex  w-full max-w-2xl  justify-center gap-2 text-xl text-barbieriBlue">
+      <div className="flex flex-wrap md:flex-nowrap w-full max-w-2xl  md:justify-center md:gap-2 gap-10 text-xl text-barbieriBlue">
         <ul className="flex w-6/12 flex-col gap-1 mt-10">
           <Link to="/tipo-de-analisis/vigas">
             <li
               className="cursor-pointer hover:font-semibold text-xl py-1"
-              onMouseOver={(e) => setUrlImg("public/vigas.png")}
-              onMouseOut={(e) => setUrlImg("public/base.png")}
+              onMouseOver={(e) => setUrlImg(imgVigas)}
+              onMouseOut={(e) => setUrlImg(imgBase)}
             >
               Vigas
             </li>
           </Link>
           <li
             className="cursor-pointer hover:font-semibold text-xl py-1"
-            onMouseOver={(e) => setUrlImg("public/vigas-tubo.png")}
-            onMouseOut={(e) => setUrlImg("public/base.png")}
+            onMouseOver={(e) => setUrlImg(imgVigasTubo)}
+            onMouseOut={(e) => setUrlImg(imgBase)}
           >
             Vigas Tubo
           </li>
           <li className="cursor-pointer hover:font-semibold text-xl py-1">Montantes</li>
         </ul>
-        <div className="border-1 w-6/12">
+        <div className="w-full md:w-6/12">
           <img src={urlImg} alt="" />
         </div>
       </div>
