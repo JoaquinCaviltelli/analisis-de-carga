0a0b82828f717f6781db37c48c58a7f3b0df101d
Joaquin <joakindel95@gmail.com>
2023-03-07 23:54:05 -0300

    bg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
+import bgHome from "/public/bg-home.jpg";
 
 const Home = () => {
   return (
-    <div className="h-screen w-full bg-[url(https://www.adbarbieri.com/hs-fs/hubfs/2022/Productos_y_Servicios/Steel_Frame/header-product.jpg?width=2000&name=header-product.jpg)] bg-cover bg-center bg-no-repeat pt-44">
-      <div className="flex flex-col items-start gap-5 p-10">
+    <div
+      className={`h-screen w-full bg-[url("/public/bg-home.jpg")] bg-cover bg-center bg-no-repeat pt-44`}
+    >
+      <div className="flex flex-col items-start gap-2 p-10">
         <p className="inline-block bg-barbieriBlue p-1 text-2xl font-light uppercase text-white ">
           Analisis de carga
         </p>
-        <p className="text-8xl font-extrabold uppercase text-white backdrop-blur py-2 px-1">
+        <p className="py-2 px-1 text-8xl font-extrabold uppercase text-white">
           Steel Frame
         </p>
         <Link to="/tipo-de-analisis">
-          <button className="mt-2 rounded bg-barbieriBlue py-2 px-10 uppercase text-white hover:bg-barbieriBlueFocus">
+          <button className="mt-10 rounded bg-barbieriBlue py-2 px-10 uppercase text-white hover:bg-barbieriBlueFocus">
             Empezar
           </button>
         </Link>
