31a4fdf37a9782a749cb81c39a53d7195db61d7f
unknown <joakindel95@gmail.com>
2023-03-10 12:54:10 -0300

    text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,14 @@ const Home = () => {
       className={`h-screen w-full bg-[url("/src/assets/bg-home.webp")] bg-cover bg-center bg-no-repeat pt-44`}
     >
       <div className="flex flex-col items-start gap-2 p-10">
-        <p className="inline-block bg-barbieriBlue p-1 text-2xl font-light uppercase text-white ">
+        <p className="inline-block bg-barbieriBlue p-1 md:text-2xl font-light uppercase text-white ">
           Analisis de carga
         </p>
-        <p className="py-2 px-1 text-8xl font-extrabold uppercase text-white">
+        <p className="py-2 px-1 text-6xl md:text-8xl font-extrabold uppercase text-white">
           Steel Frame
         </p>
         <Link to="/tipo-de-analisis">
-          <button className="mt-10 rounded bg-barbieriBlue py-2 px-10 uppercase text-white hover:bg-barbieriBlueFocus">
+          <button className="mt-3 md:mt-8 rounded bg-barbieriBlue py-2 text-xs md:text-lg px-10 uppercase text-white hover:bg-barbieriBlueFocus">
             Empezar
           </button>
         </Link>
